69db1626c922ce8d6f984c6ed4e0eae924367d02
robsonnsbr <robsonnsbr@gmail.com>
2025-02-12 15:15:13 -0300

    fix: ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,26 +14,22 @@ export default function Home() {
         className="bg-hero-section"
       >
         <Hero />
-        {/* <div className="absolute bottom-0 w-full h-7 bg-gradient-to-t from-black  to-transparent" /> */}
       </SectionContainer>
 
       <Main>
         <hr className="border-4 w-full border-lightApricotSalmon" />
         <SectionContainer id="insights-section" className="bg-lightBlueGray">
           <ContentContainer>insights</ContentContainer>
-          {/* <div className="absolute bottom-0 w-full h-7 bg-gradient-to-t from-LightGray  to-transparent" /> */}
         </SectionContainer>
 
         <hr className="border-4 w-full border-lightApricotSalmon" />
         <SectionContainer id="customer-success-section" className="bg-white ">
           <ContentContainer>CustomerSuccess</ContentContainer>
-          {/* <div className="absolute bottom-0 w-full h-7 bg-gradient-to-t from-black  to-transparent" /> */}
         </SectionContainer>
         <hr className="border-4 w-full border-lightApricotSalmon" />
 
         <SectionContainer id="form-block-section" className="bg-lightBlueGray">
           <ContentContainer>FormBlock TickerPartners</ContentContainer>
-          {/* <div className="absolute bottom-0 w-full h-7 bg-gradient-to-t from-LightGray  to-transparent" /> */}
         </SectionContainer>
         <hr className="border-4 w-full border-lightApricotSalmon" />
 
