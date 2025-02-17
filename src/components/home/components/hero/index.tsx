f12f384b71c106d33743024dd01765075ac1569a
robsonnsbr <robsonnsbr@gmail.com>
2025-02-17 20:37:24 -0300

    fix: ...

--- src/components/home/components/hero/index.tsx
+++ src/components/home/components/hero/index.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
       <div className="flex flex-col m-auto gap-16 text-black font-poppins !text-center text-balance items-center">
         <h1 className="font-extrabold leading-none uppercase">
           <span className="font-raleway flex items-center justify-center">
-            <span className="sr-only">moccelin advocacia</span>
+            <span className="sr-only">Monichara Moccelin advocacia</span>
             <Image
               alt="logo moccelin advocacia"
               src={logoNomeMoni}
