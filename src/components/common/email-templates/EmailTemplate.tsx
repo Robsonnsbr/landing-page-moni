b25eafd5aa7c378a9b87d835b8ed57286b3ed3ea
robsonnsbr <robsonnsbr@gmail.com>
2025-02-20 13:06:34 -0300

    chore: ...

--- src/components/common/email-templates/EmailTemplate.tsx
+++ src/components/common/email-templates/EmailTemplate.tsx
@@ -1,11 +1,12 @@
-import { FormData } from "src/types/formType";
+//TODO: Remover se for usar o emailjs
+import { EmailProps } from "src/types/emailsType";
 
 type EmailTemplateProps = {
-  formData: FormData;
+  formData: EmailProps;
 };
 
 export const EmailTemplate = ({ formData }: Readonly<EmailTemplateProps>) => {
-  const { name, contact } = formData;
+  const { name, email, phone, subject } = formData;
 
   const rowStyles = "border border-black px-4 py-2";
 
@@ -17,12 +18,16 @@ export const EmailTemplate = ({ formData }: Readonly<EmailTemplateProps>) => {
       <table className="w-full border-collapse mb-6 text-black">
         <tbody>
           <tr>
-            <td className={`${rowStyles} font-semibold`}>Nome Completo:</td>
+            <td className={`${rowStyles} font-semibold`}>Nome Cliente:</td>
             <td className={rowStyles}>{name}</td>
           </tr>
           <tr>
+            <td className={`${rowStyles} font-semibold`}>Assunto</td>
+            <td className={rowStyles}>{subject}</td>
+          </tr>
+          <tr>
             <td className={`${rowStyles} font-semibold`}>WhatsApp/Email:</td>
-            <td className={rowStyles}>{contact}</td>
+            <td className={rowStyles}>{email ? email : phone}</td>
           </tr>
           <tr></tr>
         </tbody>
