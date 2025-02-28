1f9b2565990d22e0e9daf3b23912dc1f08d8d458
robsonnsbr <robsonnsbr@gmail.com>
2025-02-28 14:50:26 -0300

    fix: ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,9 +40,8 @@ export const metadata: Metadata = {
   ],
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" },
+      { url: "/favicon.png", sizes: "192x192", type: "image/png" },
+      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
     ],
     apple: "/apple-touch-icon.png",
   },
@@ -64,25 +63,18 @@ export default function RootLayout({
             "https://advogadocuritiba.moccelinadvocacia.com/"
           }
         />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="48x48"
+          href="/favicon-48x48.png"
+        />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="icon" type="image/png" sizes="48x48" href="/favicon.ico" />
       </head>
       <body
         suppressHydrationWarning={true}
