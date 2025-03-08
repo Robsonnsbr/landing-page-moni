4d346ba96c027c0a457f36ead1a40f6bee7bbcb7
robsonnsbr <robsonnsbr@gmail.com>
2025-03-08 20:05:39 -0300

    fix: ...

--- src/components/home/components/info/index.tsx
+++ src/components/home/components/info/index.tsx
@@ -5,37 +5,24 @@ import { MdOutlinePhoneCallback } from "react-icons/md";
 function Info() {
   return (
     <div className="bg-deepIndigoBlue h-fit p-3 w-full flex justify-center">
-      <ul className="flex flex-wrap items-center gap-4 text-lightApricotSalmon text-base font-openSans text-center md:text-left">
+      <ul className="flex flex-wrap items-center gap-4 text-lightApricotSalmon text-base font-openSans text-center m-auto">
         <li className="w-full md:w-auto">Atendimento rápido e online:</li>
 
-        <li className="flex flex-wrap justify-center md:justify-start items-center gap-3">
-          <a href="tel:+5541998083760" className="flex items-center gap-2">
-            <MdOutlinePhoneCallback className="text-size-6" />
-            <span>Ligar</span>
-          </a>
+        <li className="flex flex-wrap justify-center  items-center gap-4 m-auto">
           <a
             href="https://api.whatsapp.com/send/?phone=5541998083760"
             className="flex items-center gap-2"
           >
-            <FaWhatsapp className="text-size-6" />
+            <FaWhatsapp className="text-xl" />
             <span>WhatsApp</span>
           </a>
-          <span className="block md:inline">(41) 99808-3760</span>
         </li>
 
-        <li className="flex flex-wrap justify-center md:justify-start items-center gap-3">
-          <a href="tel:+55419999483760" className="flex items-center gap-2">
-            <MdOutlinePhoneCallback className="text-size-6" />
-            <span>Ligar</span>
+        <li className="flex flex-wrap justify-center  items-center gap-4 m-auto">
+          <a href="tel:41999483760" className="flex items-center gap-2">
+            <MdOutlinePhoneCallback className="text-xl" />
+            <span>(41) 99948-3760</span>
           </a>
-          <a
-            href="https://api.whatsapp.com/send/?phone=55419999483760"
-            className="flex items-center gap-2"
-          >
-            <FaWhatsapp className="text-size-6" />
-            <span>WhatsApp</span>
-          </a>
-          <span className="block md:inline">(41) 99948-3760</span>
         </li>
       </ul>
     </div>
